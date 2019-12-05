ae3b6e169a89072c7c10f73d55fcde37a59a8162
Oksenyuk Dmytro <Oksenyuk.Dmitro@ukr.net>
2019-12-05 17:52:42 +0200

    fix: product mocks

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,19 +1,56 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, addMockFunctionsToSchema, MockList } from 'apollo-server';
+import faker from 'faker';
 
 import schema from './schema';
 import logger from '../utils/logger';
 import UserModel from '../models/user';
 
+const products = () => ({
+  _id: faker.random.uuid(),
+  location: faker.address.city(),
+  title: faker.random.words(),
+  price: faker.commerce.price(),
+  createdAt: faker.date.past(),
+  categoryId: faker.random.uuid(),
+  creatorId: faker.random.uuid(),
+  description: faker.random.words(),
+  category: {
+    name: faker.random.words(),
+  },
+  images: [
+    {
+      url: faker.random.image(),
+    },
+  ],
+  creator: {
+    _id: faker.random.uuid(),
+    username: faker.random.word(),
+
+    profile: {
+      firstName: faker.random.word(),
+      lastName: faker.random.word(),
+      avatar: faker.internet.avatar(),
+    },
+  },
+});
+const mocks = {
+  Query: () => ({
+    products: () => new MockList(10),
+  }),
+  Product: () => products(),
+};
 const getTokenFromReq = req => {
   const header = req.headers.authorization || '';
   const token = header.replace('Bearer ', '').replace('Token ', '');
   return token;
 };
+
+addMockFunctionsToSchema({ schema, mocks });
 const server = new ApolloServer({
   introspection: true,
   playground: true,
   schema,
-  mocks: true,
+  mocks,
   mockEntireSchema: false,
   context: async ({ req }) => {
     const token = getTokenFromReq(req);
--- src/graphql/product.js
+++ src/graphql/product.js
@@ -1,42 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { gql } from 'apollo-server';
-import faker from 'faker';
 import ProductsService from '../services/products';
 import { authCheck } from './utils';
-
-const randomArray = (min, max, callback) => {
-  const size = faker.random.number({ min, max });
-  return Array.from({ length: size }, callback);
-};
-
-const products = () => ({
-  _id: faker.random.uuid(),
-  location: faker.address.city(),
-  title: faker.random.words(),
-  price: faker.commerce.price(),
-  createdAt: faker.date.past(),
-  categoryId: faker.random.uuid(),
-  creatorId: faker.random.uuid(),
-  description: faker.random.words(),
-  category: {
-    name: faker.random.words(),
-  },
-  images: [
-    {
-      url: faker.random.image(),
-    },
-  ],
-  creator: {
-    _id: faker.random.uuid(),
-    username: faker.random.word(),
-
-    profile: {
-      firstName: faker.random.word(),
-      lastName: faker.random.word(),
-      avatar: faker.internet.avatar(),
-    },
-  },
-});
 
 export const typeDefs = gql`
   enum Status {
@@ -107,45 +72,6 @@ export const resolvers = {
       authCheck(ctx);
 
       return ProductsService.find({}, { limit, offset });
-    },
-    myProducts: (root, args, ctx) => {
-      authCheck(ctx);
-      const { limit, offset } = args;
-      return ProductsService.findByUserId(ctx.user._id, { limit, offset });
-    },
-    product: async (root, args, ctx) => {
-      authCheck(ctx);
-      const { productId } = args;
-      const product = await ProductsService.findByProductId(productId);
-      return product;
-    },
-  },
-  Mutation: {
-    addProduct: (root, args, ctx) => {
-      authCheck(ctx);
-      return ProductsService.add({
-        createdBy: ctx.user._id,
-        description: args.description,
-        location: args.location,
-        price: args.price,
-      });
-    },
-    deleteProduct: (root, args, ctx) => {
-      authCheck(ctx);
-      return ProductsService.remove(args.postId).then(result => result.ok);
-    },
-    updateProduct: (root, args, ctx) => {
-      authCheck(ctx);
-      return ProductsService.update(args.postId, args.body);
-    },
-  },
-  // TODO: implement
-};
-export const mockResolvers = {
-  Query: {
-    products: (root, args) => {
-      const { limit } = args;
-      return randomArray(limit, limit, products);
     },
     myProducts: (root, args, ctx) => {
       authCheck(ctx);
